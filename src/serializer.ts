a6570879c14b3ecb108d9d36faf6a019a5216cc7
William Lahti <wilahti@gmail.com>
2021-03-20 18:13:08 -0700

    implement parity in serializer

--- src/serializer.ts
+++ src/serializer.ts
@@ -1,5 +1,6 @@
 import { BitstreamReader, BitstreamElement, FieldDefinition, BitstreamWriter, resolveLength } from "@astronautlabs/bitstream";
 import { Serializer as ISerializer } from "@astronautlabs/bitstream";
+import { parity } from "./parity";
 
 export class Serializer implements ISerializer {
     async read(reader: BitstreamReader, type: any, parent: BitstreamElement, field: FieldDefinition): Promise<any> {
@@ -10,10 +11,17 @@ export class Serializer implements ISerializer {
         let buffer = Buffer.alloc(length);
 
         for (let i = 0; i < length; ++i) {
+            let wireParity = await reader.read(2);
             buffer[i] = await reader.read(8);
-            let parity = reader.read(2);
-
-            // TODO: handle parity
+            let expectedParity = parity(buffer[i]);
+            if (expectedParity !== wireParity) {
+                console.error(
+                    `Error: Detected incorrect parity for word ${i} ` 
+                    + `[value 0x${buffer[i].toString(16)}]: ` 
+                    + `0b${wireParity.toString(2)} ` 
+                    + `vs expected 0b${expectedParity.toString(2)}`
+                );
+            }
         }
 
         return buffer;
@@ -21,13 +29,13 @@ export class Serializer implements ISerializer {
 
     write(writer: BitstreamWriter, type: any, parent: BitstreamElement, field: FieldDefinition, buffer: any) {
         let length = resolveLength(field.length, parent, field);
-        
+
         if (!(buffer instanceof Buffer))
             throw new Error(`Value must be Buffer`);
         
         for (let i = 0; i < length; ++i) {
+            writer.write(2, parity(buffer[i]));
             writer.write(8, buffer[i]);
-            writer.write(2, 0); // TODO: generate parity
         }
     }
 }
